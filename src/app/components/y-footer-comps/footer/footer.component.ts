03acf37790cff1483a8b7f8348b869196501d015
cvara0 <cvara.parra@gmail.com>
2023-01-11 00:27:25 -0300

    version beta

--- src/app/components/y-footer-comps/footer/footer.component.ts
+++ src/app/components/y-footer-comps/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  closeResult: string='';
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+  }
+
+  open(content:any) {
+    this.modalService.open(content, {
+        ariaLabelledBy: 'modal-basic-title',
+        centered:true,
+        //size:'md'
+        //size:'lg xl sm',
+      // windowClass:'ngb-modal-style'
+        
+    }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
